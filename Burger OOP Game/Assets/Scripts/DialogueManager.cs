215852bb668fd54b2e9ac925930afcacd9a92b3d
CHOO XIANG LUN <xlchoo25@gmail.com>
2022-08-07 00:06:59 +0800

    Move external dialogue text files into script

--- Burger OOP Game/Assets/Scripts/DialogueManager.cs
+++ Burger OOP Game/Assets/Scripts/DialogueManager.cs
@@ -14,6 +14,40 @@ public class DialogueManager : MonoBehaviour
         End
     }
 
+    private readonly string[] _cnStartDialogue =
+    {
+        "“我准备好了！我准备好了！我准备好了！”",
+        "这是你在 WcD 快餐店兼职的第一天。为了要检验你这位新员工的能力，老板特别派你到厨房去制作汉堡包。",
+        "抵达厨房后，你发现它和你想象中的场景……似乎不太一致。",
+        "面对着充满 Java 代码的显示屏以及统一穿着格子衬衫的员工们，你不禁感叹：“这……我真的是在厨房吗？？”",
+        "话是这么说，但工作还是得继续做。"
+    };
+
+    private readonly string[] _cnEndDialogue =
+    {
+        "恭喜你！你已经是一名优秀的程序员……啊不，优秀的汉堡厨师了！",
+        "老板见你迅速地融入了工作氛围中，更是对你赞叹不已：“原本我还担心你之前程序员的身份让你思维转换不过来，现在看来，我是白担心了。”",
+        "……你本来想说点什么，但想想还是算了。"
+    };
+
+    private readonly string[] _enStartDialogue =
+    {
+        "\"I'm ready! I'm ready! I'm ready!\"",
+        "It's your first day working part-time at the WcD fast food restaurant.",
+        "In order to test your abilities, your boss has specially sent you to the kitchen to make hamburgers.",
+        "Upon arrival in the kitchen, you find that it does not seem to be quite the scene you imagined.",
+        "Faced with displays full of Java code and an entire staff in plaid shirts, you wonder, \"Am I ... really in the kitchen?\"",
+        "That being said, the work still needs to be done."
+    };
+
+    private readonly string[] _enEndDialogue =
+    {
+        "Congratulations! You are already an excellent programmer - I mean, an excellent burger chef!",
+        "The boss is impressed to see how quickly you've fitted into the working environment.",
+        "\"I was concerned that you might face difficulties changing your mindset due to your previous role as a programmer, but now it seems that I was worried about nothing.\"",
+        "... You were about to say something, but you thought better of it.",
+    };
+
     [SerializeField] private TextMeshProUGUI dialogueText;
     [SerializeField] private TextMeshProUGUI continueText;
     [SerializeField] private TextMeshProUGUI skipText;
@@ -60,12 +94,12 @@ public class DialogueManager : MonoBehaviour
         switch (InGameLanguage)
         {
             case GameManager.Language.English:
-                lines = System.IO.File.ReadAllLines("Assets/Dialogues/EN_StartDialogue.txt");
+                lines = _enStartDialogue;
                 continueText.text = "Continue >>";
                 skipText.text = "Skip";
                 break;
             case GameManager.Language.Chinese:
-                lines = System.IO.File.ReadAllLines("Assets/Dialogues/CN_StartDialogue.txt");
+                lines = _cnStartDialogue;
                 continueText.text = "继续 》";
                 skipText.text = "跳过";
                 break;
@@ -83,12 +117,12 @@ public class DialogueManager : MonoBehaviour
         switch (InGameLanguage)
         {
             case GameManager.Language.English:
-                lines = System.IO.File.ReadAllLines("Assets/Dialogues/EN_EndDialogue.txt");
+                lines = _enEndDialogue;
                 continueText.text = "Continue >>";
                 skipText.text = "Skip";
                 break;
             case GameManager.Language.Chinese:
-                lines = System.IO.File.ReadAllLines("Assets/Dialogues/CN_EndDialogue.txt");
+                lines = _cnEndDialogue;
                 continueText.text = "继续 》";
                 skipText.text = "跳过";
                 break;
